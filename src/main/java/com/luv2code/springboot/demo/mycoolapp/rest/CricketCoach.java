a08e1fd7ea4629e64d2e984f53740e71bba02e75
ROUBEID <OUBEID.ELHADRAMI@GMAIL.COM>
2024-06-02 15:45:06 -0400

    Update the CricketCoach.java

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/CricketCoach.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/CricketCoach.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Component;
 @Component
 public class CricketCoach implements Coach{
     @Override
-    public String getDailyWorkout() {return "This is cricket workout";}
+    public String getDailyWorkout() {return "This is cricket workout voila";}
 }
