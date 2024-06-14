b1bb5b3ab7e5ee202d19dbb959d56361dcf462f4
ROUBEID <OUBEID.ELHADRAMI@GMAIL.COM>
2024-06-13 23:31:02 -0400

    Add database mapping

--- src/main/java/com/healthai/springboot/demo/edi/DentalEDIApplication.java
+++ src/main/java/com/healthai/springboot/demo/edi/DentalEDIApplication.java
@@ -1,13 +1,23 @@
 package com.healthai.springboot.demo.edi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DentalEDIApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DentalEDIApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		return runner -> {
+			System.out.println("Hello World");
+		};
 	}
 
 }
